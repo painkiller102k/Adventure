6acd6ca532def4efb2f7f2967c7d7f9742e68e8a
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-10-06 09:30:06 +0300

    Update funktsioonid.sql
    teine funktsioon

--- funktsioonid.sql
+++ funktsioonid.sql
@@ -1,6 +1,6 @@
 select * from DimEmployee
 
---Tabelisisev‰‰rtusega funktsioon e Inline Table Valued function (ILTVF) koodin‰ide:
+--Tabelisisev√§√§rtusega funktsioon e Inline Table Valued function (ILTVF) koodin√§ide:
 create function fn_iltvf_employees()
 returns Table
 as
@@ -8,4 +8,18 @@ return (select employeekey,firstname,
 cast(birthdate as date) as DOB
 from dbo.DimEmployee);
 --kontroll
+
 select * from fn_iltvf_employees();
+
+--Mitme avaldisega tabeliv√§√§rtusega funktsioonid e multi-statement table valued function (MSTVF):
+create function fn_mstvf_getemployees()
+returns @Table table (id int,Name nvarchar(20), DOB date)
+as
+begin
+insert into @Table
+select Id,Name,cast(DateOfBirth as date)
+from tblEmployees
+return
+end
+--kontroll
+Select * from fn_MSTVF_GetEmployees()
