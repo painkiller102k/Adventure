ce82975fd3ea28fb95c2f2258258d2264ca0ba33
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-10-09 14:00:05 +0300

    view sql serveris nr 39 ; Lisatud on vaated (views) töötajate ja osakondade andmete ühendamiseks JOIN-i abil: - vWEmployeeByDepartment: töötajate nimekiri koos osakondade andmetega - vWITManufacturingDepartment_Employees: tootmisosakonna töötajad - vWEmployeesNonConfData: töötajad, kellel puuduvad konfidentsiaalsed andmed (ilma BaseRate'ita) - vWEmployeesCountByDepartment: töötajate koguarv osakondade kaupa

--- viewserver.sql
+++ viewserver.sql
@@ -1,17 +1,58 @@
 select * from DimDepartmentGroup
 
---Selleks, et saada soovitud tulemus, me peaksime �hendama kaks tabelit omavahel
+--Selleks, et saada soovitud tulemus, me peaksime ühendama kaks tabelit omavahel
 --Kui JOIN-d on sulle uus teema, siis vaata eelnevaid harjutusi JOIN-de kohta.
 SELECT EmployeeKey, FirstName, BaseRate, Gender, DepartmentName
 FROM DimEmployee
 JOIN DimDepartmentGroup 
 ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupName
 
---N��d loome view, kus kasutame JOIN-i:
+--Nüüd loome view, kus kasutame JOIN-i:
 CREATE VIEW vWEmployeeByDepartment
 AS
 SELECT EmployeeKey, FirstName, BaseRate, DepartmentName
 FROM DimEmployee
 JOIN DimDepartmentGroup
 ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupKey
+--kontroll
+select * from vWEmployeeByDepartment
+
+-- View, mis tagastab ainult Manufacturing osakonna töötajad
+CREATE VIEW vWITManufacturingDepartment_Employees
+AS
+SELECT EmployeeKey, FirstName, BaseRate, DepartmentName
+FROM DimEmployee
+JOIN DimDepartmentGroup
+ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupKey
+WHERE DimDepartmentGroup.DepartmentGroupName = 'Manufacturing'
+--kontroll
+SELECT * FROM vWITManufacturingDepartment_Employees
+
+--View, kus ei ole BaseRate veergu
+CREATE VIEW vWEmployeesNonConfData
+AS
+SELECT EmployeeKey, FirstName, Gender, DepartmentName
+FROM DimEmployee
+JOIN DimDepartmentGroup
+ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupKey
+--kontroll
+SELECT * FROM vWEmployeesNonConfData
+
+--View, mis tagastab summeeritud andmed töötajate koondarvest.
+CREATE VIEW vWEmployeesCountByDepartment
+AS
+SELECT DepartmentName, COUNT(DepartmentGroupKey) AS TotalEmployees
+FROM DimEmployee
+JOIN DimDepartmentGroup
+ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupKey
+GROUP BY DepartmentName
+--kontroll
+SELECT * FROM vWEmployeesCountByDepartment
+
+-- Kui soovid vaadata view definitsiooni
+sp_helptext vWName
+-- Kui soovid muuta view-d
+ALTER VIEW
+-- Kui soovid kustutada view-d 
+DROP VIEW vWName
 
